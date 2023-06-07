3a6f6d1d7adb84fcfd614c7dab25a72969cf75c4
choi <choi@choiui-noteubug.local>
2023-06-07 16:45:14 +0900

    update 4

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -2,8 +2,8 @@ module "req_accnt_dev01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "a01091680906+ctws-aftdev3@gmail.com"
-    AccountName  = "accRqTestHanseo3"
+    AccountEmail = "a01091680906+ctws-aftdev4@gmail.com"
+    AccountName  = "accRqTestHanseo4"
     # Syntax for top-level OU
     ManagedOrganizationalUnit = "ProductionVersion2"
     # Syntax for nested OU
