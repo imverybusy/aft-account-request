4007d3d6c27a524c37cb86b221fc06fbe7219b92
choi <choi@choiui-noteubug.local>
2023-06-07 10:27:19 +0900

    update

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -4,29 +4,9 @@ module "req_accnt_dev01" {
   control_tower_parameters = {
     AccountEmail = "a01091680906+ctws-aftdev1@gmail.com"
     AccountName  = "accRqTestHanseo"
-    # Syntax for top-level OU
     ManagedOrganizationalUnit = "ProductionVersion2"
-    # Syntax for nested OU
-    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
     SSOUserEmail     = "a01091680906@gmail.com"
     SSOUserFirstName = "hanseo"
     SSOUserLastName  = "choi"
   }
-
-  # account_tags = {
-  #   "ABC:Owner"       = "woojun@amazon.com"
-  #   "ABC:Environment" = "Dev"
-  #   "ABC:Project"     = "CTWS-AFT"
-  # }
-
-  # change_management_parameters = {
-  #   change_requested_by = "Jonas Woo"
-  #   change_reason       = "Customized dev account vending with AFT"
-  # }
-
-  # custom_fields = {
-  #   custom_param1 = "dev1"
-  # }
-
-  # account_customizations_name = "DEV2-BUDGET-2"
 }
