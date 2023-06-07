b88edd72d3104e07a6b686fa52dea34050902022
choi <choi@choiui-noteubug.local>
2023-06-07 10:24:38 +0900

    update user account

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -2,31 +2,31 @@ module "req_accnt_dev01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "woojun+ctws-aftdev1@amazon.com"
-    AccountName  = "aftdev1"
+    AccountEmail = "a01091680906+ctws-aftdev1@gmail.com"
+    AccountName  = "accRqTestHanseo"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "Sandbox"
+    ManagedOrganizationalUnit = "ProductionVersion2"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "woojun@amazon.com"
-    SSOUserFirstName = "Jonas"
-    SSOUserLastName  = "Woo"
+    SSOUserEmail     = "a01091680906@gmail.com"
+    SSOUserFirstName = "hanseo"
+    SSOUserLastName  = "choi"
   }
 
-  account_tags = {
-    "ABC:Owner"       = "woojun@amazon.com"
-    "ABC:Environment" = "Dev"
-    "ABC:Project"     = "CTWS-AFT"
-  }
+  # account_tags = {
+  #   "ABC:Owner"       = "woojun@amazon.com"
+  #   "ABC:Environment" = "Dev"
+  #   "ABC:Project"     = "CTWS-AFT"
+  # }
 
-  change_management_parameters = {
-    change_requested_by = "Jonas Woo"
-    change_reason       = "Customized dev account vending with AFT"
-  }
+  # change_management_parameters = {
+  #   change_requested_by = "Jonas Woo"
+  #   change_reason       = "Customized dev account vending with AFT"
+  # }
 
-  custom_fields = {
-    custom_param1 = "dev1"
-  }
+  # custom_fields = {
+  #   custom_param1 = "dev1"
+  # }
 
-  account_customizations_name = "DEV2-BUDGET-2"
+  # account_customizations_name = "DEV2-BUDGET-2"
 }
